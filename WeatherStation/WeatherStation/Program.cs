a81cd193ff7d28716e7da42a6bf8ab16e605cadb
Ksenia <ichanskij@gmail.com>
2019-03-24 12:39:08 +0200

    add WeatherCity

--- WeatherStation/WeatherStation/Program.cs
+++ WeatherStation/WeatherStation/Program.cs
@@ -11,18 +11,24 @@ namespace WeatherStation
     {
         static void Main(string[] args)
         {
-            var client = new OpenWeatherAPI("eb873a38c50a5a2519861054b5cbaae1");
-
+            Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("OpenWeatherAPI Example Application");
             Console.WriteLine();
 
             Console.WriteLine("Виберіть місто для перегляду погоди:");
-            var city = Console.ReadLine();
+            //var city = Console.ReadLine();
+            string city1 = "Hrebinka";
+            string city2 = "Kiev";
+            string city3 = "London";
             Console.WriteLine();
 
-            var results = client.Query(city);
+            WeatherCity weather1 = new WeatherCity(city1);
+            WeatherCity weather2 = new WeatherCity(city2);
+            WeatherCity weather3 = new WeatherCity(city3);
 
-            Console.WriteLine($"Температура в {city} = {results.Main.Temperature.CelsiusCurrent}C, вітер {results.Wind.SpeedMetersPerSecond} м/с, хмарність {results.Clouds.All}%.");
+            Console.WriteLine(weather1);
+            Console.WriteLine(weather2);
+            Console.WriteLine(weather3);
 
             Console.ReadLine();
         }
---
+++ WeatherStation/WeatherStation/WeatherCity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStation
+{
+    class WeatherCity
+    {
+        public readonly string City;
+        public readonly double CelsiusCurrent;
+        public readonly double SpeedMetersPerSecond;
+        public readonly double Clouds;
+
+        public WeatherCity(string city)
+        {
+            OpenWeatherAPI client = new OpenWeatherAPI("eb873a38c50a5a2519861054b5cbaae1");
+            Query results = client.Query(city);
+            City = city;
+            CelsiusCurrent = results.Main.Temperature.CelsiusCurrent;
+            SpeedMetersPerSecond = results.Wind.SpeedMetersPerSecond;
+            Clouds = results.Clouds.All;
+        }
+
+        public override string ToString()
+        {
+            return $"Температура в {City} = {CelsiusCurrent}C, вітер {SpeedMetersPerSecond} м/с, хмарність {Clouds}%.";
+        }
+    }
+}
